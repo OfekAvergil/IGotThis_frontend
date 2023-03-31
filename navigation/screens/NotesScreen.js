456b11279b40adbbd902bf245fce9ecd223f7061
reutdayan <reutdayan1@gmail.com>
2023-03-31 11:34:39 +0300

    notes, add new notes

--- navigation/screens/NotesScreen.js
+++ navigation/screens/NotesScreen.js
@@ -1,15 +1,66 @@
-import * as React from "react";
-import { View, Text } from "react-native";
+import React, { useState } from 'react';
+import { View, Text, TextInput, TouchableOpacity, StyleSheet, FlatList } from 'react-native';
 
-export default function NotesScreen({ navigation }) {
+export default function NotesScreen() {
+  const [notes, setNotes] = useState([]);
+  const [newNote, setNewNote] = useState('');
+
+  const addNote = () => {
+    setNotes([...notes, newNote]);
+    setNewNote('');
+  };
+
+  const renderItem = ({ item }) => (
+    <View style={styles.item}>
+      <Text>{item}</Text>
+    </View>
+  );
+
   return (
-    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-      <Text
-        onPress={() => navigation.navigate('Home')}
-        style={{ fontSize: 30 }}
-      >
-        Notes Screen
-      </Text>
+    <View style={styles.container}>
+      <TextInput
+        style={styles.input}
+        onChangeText={(text) => setNewNote(text)}
+        value={newNote}
+        placeholder="Add a new note..."
+      />
+      <TouchableOpacity style={styles.addButton} onPress={addNote}>
+        <Text style={styles.addButtonText}>Add</Text>
+      </TouchableOpacity>
+      <FlatList
+        data={notes}
+        renderItem={renderItem}
+        keyExtractor={(item, index) => index.toString()}
+      />
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 20,
+    backgroundColor: '#fff',
+  },
+  input: {
+    borderWidth: 1,
+    borderColor: '#ccc',
+    padding: 10,
+    marginBottom: 10,
+  },
+  addButton: {
+    backgroundColor: '#007AFF',
+    padding: 10,
+    borderRadius: 5,
+    alignItems: 'center',
+  },
+  addButtonText: {
+    color: '#fff',
+  },
+  item: {
+    borderWidth: 1,
+    borderColor: '#ccc',
+    padding: 10,
+    marginBottom: 10,
+  },
+});
